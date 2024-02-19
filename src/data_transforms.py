b3966c1674475a12260a2790c2663b835865ab8b
Rej <rej.safranko@gmail.com>
2024-02-19 14:20:21 -0800

    dates cleaning functions

--- src/data_transforms.py
+++ src/data_transforms.py
@@ -1,7 +1,55 @@
 import pandas as pd
 
 
-def handle_overtime_cancellation(df: pd.DataFrame):
+# RULE 1
+def handle_prereservation_arrival(df: pd.Series):
+    df["datum_dolaska"] = pd.to_datetime(df["datum dolaska"])
+    df["datum_kreiranja_rezervacije"] = pd.to_datetime(
+        df["datum_kreiranja_rezervacije"]
+    )
+    return df[
+        df["datum_dolaska"] >= df["datum_kreiranja_rezervacije"]
+    ]  # Filter rows where arrival date is on or after reservation creation date.
+
+
+# RULE 2
+def handle_prearrival_checkout(df: pd.Series):
+    df["datum_dolaska"] = pd.to_datetime(df["datum dolaska"])
+    df["datum_odjave"] = pd.to_datetime(df["datum_odjave"])
+    return df[df["datum_dolaska"] <= df["datum_odjave"]]
+
+
+# RULE 3
+def handle_prereservation_cancellation(df):
+    df["datum_kreiranja_rezervacije"] = pd.to_datetime(
+        df["datum_kreiranja_rezervacije"]
+    )
+    df["datum_otkazivanja_rezervacije"] = pd.to_datetime(
+        df["datum_otkazivanja_rezervacije"]
+    )
+    condition = (df["datum_otkazivanja_rezervacije"].isnull()) | (
+        df["datum_otkazivanja_rezervacije"] > df["datum_kreiranja_rezervacije"]
+    )
+    return df[condition]
+
+
+# RULE 4
+
+
+# case 1
+def handle_overtime_cancellation_drop(df: pd.Series):
+    df["datum_odjave"] = pd.to_datetime(df["datum_odjave"])
+    df["datum_otkazivanja_rezervacije"] = pd.to_datetime(
+        df["datum_otkazivanja_rezervacije"]
+    )
+    condition = (df["datum_otkazivanja_rezervacije"].isnull()) | (
+        df["datum_otkazivanja_rezervacije"] < df["datum_odjave"]
+    )
+    return df[condition]
+
+
+# case 2
+def handle_overtime_cancellation_cast(df: pd.DataFrame):
     df["datum_otkazivanja_rezervacije"] = pd.to_datetime(
         df["datum_otkazivanja_rezervacije"]
     )
