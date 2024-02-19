5921222c2a37a6b52e879fc4143bb49c520fb3d2
Rej <rej.safranko@gmail.com>
2024-02-19 11:19:14 -0800

    data transform rule one

---
+++ src/data_transforms.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def handle_overtime_cancellation(df: pd.DataFrame):
+    df["datum_otkazivanja_rezervacije"] = pd.to_datetime(
+        df["datum_otkazivanja_rezervacije"]
+    )
+    df["datum_odjave"] = pd.to_datetime(df["datum_odjave"])
+    condition = df["datum_otkazivanja_rezervacije"] > df["datum_odjave"]
+    df.loc[condition, "status_rezervacije"] = "check-out"
+    df.loc[condition, "datum_otkazivanja_rezervacije"] = None
+    return df
